182446ee30814b4f3e90f2e80300661fbfcfe361
pavel <p97p@ya.ru>
2019-01-15 22:03:11 +0300

    request permission and listen location updates

--- app/src/main/java/p/testtimer/MainActivity.kt
+++ app/src/main/java/p/testtimer/MainActivity.kt
@@ -1,12 +1,44 @@
 package p.testtimer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.nlopez.smartlocation.SmartLocation
+import io.nlopez.smartlocation.location.config.LocationParams
+import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
 
+
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // NOTE: delegate the permission handling to generated function
+        startListeningLocationWithPermissionCheck()
+    }
+
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun startListeningLocation() {
+
+        SmartLocation.with(this)
+            .location()
+            .continuous()
+            .config(LocationParams.NAVIGATION)
+            .start {
+                Log.d("QWE", "$it")
+                textView.text = "$it"
+            }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // NOTE: delegate the permission handling to generated function
+        onRequestPermissionsResult(requestCode, grantResults)
     }
 }
